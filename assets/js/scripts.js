d6403aa3afcf905fff699b8f1bb73c9d428c4f5a
Terenceonye <onyeweketerence@gmail.com>
2024-11-04 17:20:28 +0100

    final for production

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,33 @@
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-button");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+
+// Script to open and close navbar
+function openNavba() {
+   document.getElementById("navba-links").classList.add('show-navba');
+}
+
+function closeNavba() {
+   document.getElementById("navba-links").classList.remove('show-navba');
+}
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', ()=> {
 
 
